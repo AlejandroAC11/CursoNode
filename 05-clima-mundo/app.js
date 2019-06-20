dde1386af40fcd08a869e7f3f98399a6b03dbd60
Alejandro Álvarez <alejandro.alvarez@redegal.com>
2019-06-20 11:54:49 +0200

    ejercicio 5

---
+++ 05-clima-mundo/app.js
@@ -0,0 +1,28 @@
+const lugar = require('./lugar/lugar');
+const clima = require('./clima/clima');
+
+const argv = require('yargs').options({
+    direccion: {
+        alias: 'd',
+        desc: 'Direccion ciudad para obtener el clima',
+        demand: true
+    }
+}).argv;
+
+// lugar.getLugarLatLng(argv.direccion).then(data => console.log(data));
+// clima.getClima().then(data => console.log(data));
+
+
+
+const getInfo = async( direccion ) => {
+    try {
+        const coords = await lugar.getLugarLatLng(direccion);
+        const temp = await clima.getClima(coords.lat, coords.lng);
+
+        return `El clima de ${ direccion } es de ${ temp }`;
+    } catch (e) {
+        return "Error al obtener clima";
+    }
+}
+
+getInfo(argv.direccion).then(console.log).catch(console.log);
